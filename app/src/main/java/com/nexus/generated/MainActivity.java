292c2cc46d3016bc786f9a7f7f07d73c0087bdb7
phamcucpqrwbbnposhmn96-ui <phamcucpqrwbbnposhmn96@gmail.com>
2026-07-20 18:33:28 +0800

    Update app/src/main/java/com/nexus/generated/MainActivity.java

---
+++ app/src/main/java/com/nexus/generated/MainActivity.java
@@ -0,0 +1,9 @@
+package com.nexus.generated;
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity {
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(android.R.layout.activity_list_item);
+  }
+}
